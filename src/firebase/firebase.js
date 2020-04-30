380f8cfe67f1a93fc6e0a673506bc0fd6401dc2e
Antonio Frigo <frigo.antonio.mct@gmail.com>
2020-04-30 10:45:27 -0400

    Initial Commit

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+const onfig = {
+  apiKey: "AIzaSyCW_GFntr4Fwe0ajuEzBVepn1FEKP7Lir0",
+  authDomain: "icebox-4f67e.firebaseapp.com",
+  databaseURL: "https://icebox-4f67e.firebaseio.com",
+  projectId: "icebox-4f67e",
+  storageBucket: "icebox-4f67e.appspot.com",
+  messagingSenderId: "360264376419",
+  appId: "1:360264376419:web:d9314c2b373f569ef55856"
+};
+firebase.initializeApp(config);
+export default firebase;
---
+++ src/viewer/Viewer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Viewer extends Component {
+	
+	render () {
+		return(
+			<div>
+
+			</div>
+			)
+	}
+}
+
+export default Viewer
